542a798010f9a24c3883d9bab9fe01af1c06b13f
ARatThiLoan12 <54876040+ARatThiLoan12@users.noreply.github.com>
2019-11-05 17:36:25 +0700

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -21,7 +21,6 @@ var s1 = {
     name: "Ngủ cùng sói",
     img: "../IMG/ngu-cung-soi.jpg",
     author: "Diệp Lạc Vô Tâm",
-    rate: "5/5",
     sum: 'Truyện Ngủ Cùng Sói của tác giả  Diệp Lạc Vô Tâm đem lại cho ta nhiều cảm xúc khác nhau, nhẹ nhàng nhưng sâu lắng. Để trả thù cho đại ca, Hàn Trạc Thần đã giết cả nhà của tên lái xe, cũng chính là kẻ phản bội, nhưng lại tha chết cho đứa con gái mới 7 tuổi của hắn.Hai năm sau, Hàn Trạc Thần đến viện phúc lợi và nhận nuôi hai đứa con, nhưng chỉ giữ lại đứa con gái và dặt tên là Hàn Thiên Vu. Hàn Thiên Vu đã sưởi ấm trái tim băng giá ấy bằng tình cảm gia đình mà trước kia, hắn chưa bao giờ cảm nhận được một cách trọn vẹn. Một bên tình một bên thù liệu bên nào sẽ lấn áp hơn đây?',
     chap1: 'Người ta thường nói: “Phụ nữ như cây dương cầm, nếu được nghệ sĩ tài ba diễn tấu sẽ tạo nên một bản nhạc hay'
     +' nếu vào tay nghệ sĩ tầm thường, chí ít cũng còn là một bản nhạc, nhưng nếu gặp phải người không biết chơi đàn, e rằng không còn là nhạc nữa...”'
@@ -36,7 +35,6 @@ var s2 = {
     name: "Mãi mãi là bao xa",
     img: "../IMG/715.jpg",
     author: "Diệp Lạc Vô Tâm",
-    rate: "5/5",
     sum: 'Bạch Lăng Lăng, nữ sinh khoa Điện khí, trẻ trung, xinh đẹp và rất tự hào khi quen được một người bạn lý tưởng qua mạng, chàng du học sinh của một trường nổi tiếng của Mỹ, người mang biệt danh “nhà khoa học”: Mãi Mãi Là Bao Xa. Qua những cuộc chuyện trò trên QQ, Lăng Lăng đã gắn bó với chàng trai đó lúc nào cô cũng không hay, cảm xúc lớn dần, sự chia sẻ lớn dần và đến một ngày cô phát hiện ra mình đã yêu người con trai “tài giỏi” và không một chút khuyết điểm ấy. Nhưng sự tỉnh táo giúp cô ý thức được rằng, thế giới mạng chỉ là ảo, họ ở cách nhau cả một đại dương mênh mông, anh lại quá xuất sắc và ưu tú, mối quan hệ của họ sẽ không có kết quả gì. Nhất là khi anh thông báo, nếu anh tiếp tục sự nghiệp nghiên cứu lần này, rất có thể anh phải định cư bên Mỹ, mãi mãi không trở về. Khi nghe tin đó, cô đã gục xuống trước màn hình máy tính và khóc. Tất cả như sụp đổ, tia hy vọng cuối cùng dập tắt, anh sẽ không về nước nữa, khoảng cách giữa họ là mãi mãi… Cô cay đắng nói với anh lời từ biệt và đưa nick của anh vào danh sách đen, không bao giờ xuất hiện khi cô đăng nhập QQ nữa…',
     chap1: 'Người ta thường nói: “Phụ nữ như cây dương cầm, nếu được nghệ sĩ tài ba diễn tấu sẽ tạo nên một bản nhạc hay'
     +' nếu vào tay nghệ sĩ tầm thường, chí ít cũng còn là một bản nhạc, nhưng nếu gặp phải người không biết chơi đàn, e rằng không còn là nhạc nữa...”'
@@ -51,7 +49,6 @@ var s3 = {
     name: "Hai lần gặp gỡ",
     img: "../IMG/hai-lan-gap-go.jpg",
     author: "Đinh Mặc",
-    rate: "5/5",
     sum: 'Số phận – tựa như một vòng tròn vô hình – đã gói trọn họ lại với nhau như thế.Nàng – vẫn tưởng như trái tim đã bình lặng sau khi mối tình đầu tan vỡ, lại không ngờ số phận nghiệt ngã khiến nàng gặp anh – người nàng không nên yêu.Anh – vẫn đau đáu vì quá khứ ngủ quên của mình, vì nỗi buồn không sao gọi thành lời, trốn chạy tình yêu của nàng, để đến khi đi tới tận cùng ánh sáng mới nhận ra – anh còn nợ nàng một câu: “không phải không yêu”.Cậu – đau khổ khi tình thân dần biến thành tình yêu, khi hờn giận trở thành cuồng nộ, đánh đổi tất cả chỉ để níu lấy một ánh mắt để rồi khi có được lại cay đắng nhận ra: “nàng”... đã không còn là nàng nữa.',
     chap1: 'Người ta thường nói: “Phụ nữ như cây dương cầm, nếu được nghệ sĩ tài ba diễn tấu sẽ tạo nên một bản nhạc hay'
     +' nếu vào tay nghệ sĩ tầm thường, chí ít cũng còn là một bản nhạc, nhưng nếu gặp phải người không biết chơi đàn, e rằng không còn là nhạc nữa...”'
@@ -66,7 +63,6 @@ var s4 = {
     name: "Archimedes thân yêu",
     img: "../IMG/archimedes-than-yeu.jpg",
     author: "Đinh Mặc",
-    rate: "5/5",
     sum: 'Chân Ái với vẻ không tin nổi, "Anh lại đặt tên cho thú cưng bằng tên của Einstein và Isaac Newton sao?""Mặc dù tôi đánh giá cao việc cô đã nhận ra nguồn gốc tên của các cô nàng nhưng tôi không thích thái độ của cô với chúng." Ngôn Tố kiêu căng hất mặt, giọng đầy bất mãn. "Albert là con cá nhiệt đới rất có trí tuệ, còn Isaac thuộc được ba định luật Newton bằng tiếng Anh, khẩu âm quận Devon nước Anh. Còn nữa, nó rất thích ăn táo."Chân Ái gật đầu: "Anh chọn Marie làm nữ giúp việc không phải vì tên cô ấy giống với Marie Curie chứ?"Ngôn Tố nheo mắt nhìn cô một lúc, mím môi: "Cô thông minh hơn tôi tưởng. OK, cô có thể tá túc ở nhà',
     chap1: 'Người ta thường nói: “Phụ nữ như cây dương cầm, nếu được nghệ sĩ tài ba diễn tấu sẽ tạo nên một bản nhạc hay'
     +' nếu vào tay nghệ sĩ tầm thường, chí ít cũng còn là một bản nhạc, nhưng nếu gặp phải người không biết chơi đàn, e rằng không còn là nhạc nữa...”'
@@ -94,9 +90,39 @@ function displayq(storiesArray) {
         var a = document.createElement("p");
         a.className = "author";
         a.innerText = storiesArray[i].author;
-        var r = document.createElement("p");
-        r.innerText = storiesArray[i].rate;
-        r.className = "rate";
+
+        var rateline = document.createElement("span");
+        //rate1
+        //var ratebtn1 = document.createElement("button");
+        var rateicon1 = document.createElement("i");
+        rateicon1.className = "far fa-star";
+        //ratebtn1.append(rateicon1);
+        //rate2
+        //var ratebtn2 = document.createElement("button");
+        var rateicon2 = document.createElement("i");
+        rateicon2.className = "far fa-star";
+        //ratebtn2.append(rateicon2);
+        //rate3
+        //var ratebtn3 = document.createElement("button");
+        var rateicon3 = document.createElement("i");
+        rateicon3.className = "far fa-star";
+        //ratebtn3.append(rateicon3);
+        //rate4
+        //var ratebtn4 = document.createElement("button");
+        var rateicon4 = document.createElement("i");
+        rateicon4.className = "far fa-star";
+        //ratebtn4.append(rateicon4);
+        //rate5
+        //var ratebtn5 = document.createElement("button");
+        var rateicon5 = document.createElement("i");
+        rateicon5.className = "far fa-star";
+        //ratebtn5.append(rateicon5);
+        rateline.append(rateicon1);
+        rateline.append(rateicon2);
+        rateline.append(rateicon3);
+        rateline.append(rateicon4);
+        rateline.append(rateicon5); 
+        
         var read = document.createElement("button");
         read.innerText = "Đọc truyện";
         read.className = "read";
@@ -121,19 +147,13 @@ function displayq(storiesArray) {
                 var rating = document.createElement("span");
                 var rate = document.createElement("button");
                 rate.innerText = "Đánh giá truyện";
-                rate.onclick = function(arg){
-                    return function(){
-                        var demo='<h1>Star Rating System</h1>';
-                        demo+='<div class="stars" data-rating="3">';
-                        demo+='<span class="star">&nbsp;</span>';
-                        demo+='<span class="star">&nbsp;</span>';
-                        demo+='<span class="star">&nbsp;</span>';
-                        demo+='<span class="star">&nbsp;</span>';
-                        demo+='<span class="star">&nbsp;</span>';
-                        demo+='</div>'; 
-                    }
-                    rating.innerHTML = demo;
-                }
+                // rate.onclick = function(arg){
+                //     return function(){
+                //         var span = document.createElement("div");
+                        
+                //     }
+                // }
+                
                 var tap = document.createElement("p");
                 var tap1 = document.createElement("button");
                 tap1.innerText = "Chương 1";
@@ -154,7 +174,6 @@ function displayq(storiesArray) {
                 box.appendChild(im);
                 box.appendChild(n);
                 box.appendChild(a);
-                box.appendChild(r);
                 box.appendChild(summary);
                 box.appendChild(rate);
                 box.appendChild(tap);
@@ -173,7 +192,7 @@ function displayq(storiesArray) {
             }
         }(i);
         box.appendChild(n);
-        box.appendChild(r);
+        box.appendChild(rateline);
         box.appendChild(read);
         box.appendChild(love);
         container.appendChild(box);
